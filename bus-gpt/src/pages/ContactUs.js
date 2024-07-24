79282134d914c0000cd74fd26d465751c4b836cc
Amy <dragoamy@gmail.com>
2024-07-24 23:13:11 +0800

    Changed to usenavigate

--- bus-gpt/src/pages/ContactUs.js
+++ bus-gpt/src/pages/ContactUs.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import '../App.css';
 
 const ContactUs = () => {

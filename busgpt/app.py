3f5a3375a4a7693de0f7c4c0590b007e9e340196
Amy <dragoamy@gmail.com>
2024-07-26 00:40:51 +0800

    Python again

---
+++ busgpt/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, redirect
+import pymysql
+from config import DB_CONFIG
+
+app = Flask(__name__)
+
+# Database connection
+def get_db_connection():
+    return pymysql.connect(
+        host=DB_CONFIG['host'],
+        user=DB_CONFIG['user'],
+        password=DB_CONFIG['password'],
+        database=DB_CONFIG['database'],
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/contact_us', methods=['GET', 'POST'])
+def contact_us():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+        connection = get_db_connection()
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO contacts (name, email, message) VALUES (%s, %s, %s)"
+            cursor.execute(sql, (name, email, message))
+        connection.commit()
+        connection.close()
+
+        return redirect('/')
+
+    return render_template('contact_us.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
